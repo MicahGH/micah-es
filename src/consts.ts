f1f43c65cdbd5424ac5ffb37dd20d94586aa7841
Micah Shiels <micah.shiels@placeholder.es>
2026-06-25 21:45:01 +0200

    Finish Skills section

---
+++ src/consts.ts
@@ -0,0 +1,184 @@
+import Docker from "./icons/Docker.astro"
+import Django from "./icons/Django.astro"
+import PostgreSQL from "./icons/PostgreSQL.astro"
+import Python from "./icons/Python.astro"
+import Poetry from "./icons/Poetry.astro"
+import Ass from "./icons/Ass.astro"
+import Tailwind from "./icons/Tailwind.astro"
+import HTML from "./icons/HTML.astro"
+import CSS from "./icons/CSS.astro"
+import MicrosoftSQLServer from "./icons/MicrosoftSQLServer.astro"
+import CPlusPlus from "./icons/CPlusPlus.astro"
+import TypeScript from "./icons/TypeScript.astro"
+import JavaScript from "./icons/JavaScript.astro"
+import FastAPI from "./icons/FastAPI.astro"
+import VBA from "./icons/VBA.astro"
+import SQL from "./icons/SQL.astro"
+import PLSQL from "./icons/PLSQL.astro"
+import Neo4j from "./icons/Neo4j.astro"
+import MongoDB from "./icons/MongoDB.astro"
+import MySQL from "./icons/MySQL.astro"
+import SQLite from "./icons/SQLite.astro"
+import Redis from "./icons/Redis.astro"
+import GitHub from "./icons/GitHub.astro"
+import GCloud from "./icons/GCloud.astro"
+import Sentry from "./icons/Sentry.astro"
+import Jira from "./icons/Jira.astro"
+import Retool from "./icons/Retool.astro"
+import Streamlit from "./icons/Streamlit.astro"
+import Polars from "./icons/Polars.astro"
+import Pandas from "./icons/Pandas.astro"
+import SQLAlchemy from "./icons/SQLAlchemy.astro"
+import BeautifulSoup from "./icons/BeautifulSoup.astro"
+import Pydantic from "./icons/Pydantic.astro"
+
+export const TAGS = {
+    PYTHON: {
+        name: "Python",
+        icon: Python,
+    },
+    DJANGO: {
+        name: "Django",
+        icon: Django,
+    },
+    POSTGRESQL: {
+        name: "PostgreSQL",
+        icon: PostgreSQL,
+    },
+    DOCKER: {
+        name: "Docker",
+        icon: Docker,
+    },
+    POETRY: {
+        name: "Poetry",
+        icon: Poetry,
+    },
+    ASTRO: {
+        name: "Astro",
+        icon: Ass,
+    },
+    TAILWIND: {
+        name: "Tailwind",
+        icon: Tailwind,
+    },
+    HTML: {
+        name: "HTML",
+        icon: HTML,
+    },
+    CSS: {
+        name: "CSS",
+        icon: CSS,
+    },
+    MICROSOFTSQLSERVER: {
+        name: "Microsoft SQL Server",
+        icon: MicrosoftSQLServer,
+    },
+    CPLUSPLUS: {
+        name: "C++",
+        icon: CPlusPlus,
+    },
+    TYPESCRIPT: {
+        name: "TypeScript",
+        icon: TypeScript
+    },
+    JAVASCRIPT: {
+        name: "JavaScript",
+        icon: JavaScript
+    },
+    VBA: {
+        name: "VBA",
+        icon: VBA
+    },
+    FASTAPI: {
+        name: "FastAPI",
+        icon: FastAPI
+    },
+    SQL: {
+        name: "SQL",
+        icon: SQL
+    },
+    PLSQL: {
+        name: "PL/SQL",
+        icon: PLSQL
+    },
+    NEO4J: {
+        name: "Neo4j",
+        icon: Neo4j
+    },
+    PT: {
+        name: "Português (B1 / Intermediate)",
+        icon: "🇵🇹"
+    },
+    GB: {
+        name: "English (Native)",
+        icon: "🇬🇧"
+    },
+    ES: {
+        name: "Español (C2 / Mastery)",
+        icon: "🇪🇸"
+    },
+    DE: {
+        name: "Deutsch (A1 / Beginner)",
+        icon: "🇩🇪"
+    },
+    MONGODB: {
+        name: "MongoDB",
+        icon: MongoDB
+    },
+    MYSQL: {
+        name: "MySQL",
+        icon: MySQL
+    },
+    SQLITE: {
+        name: "SQLite",
+        icon: SQLite
+    },
+    REDIS: {
+        name: "Redis",
+        icon: Redis
+    },
+    GITHUB: {
+        name: "GitHub",
+        icon: GitHub
+    },
+    GCLOUD: {
+        name: "Google Cloud",
+        icon: GCloud
+    },
+    SENTRY: {
+        name: "Sentry",
+        icon: Sentry
+    },
+    JIRA: {
+        name: "Jira",
+        icon: Jira
+    },
+    RETOOL: {
+        name: "Retool",
+        icon: Retool
+    },
+    STREAMLIT: {
+        name: "Streamlit",
+        icon: Streamlit
+    },
+    POLARS: {
+        name: "Polars",
+        icon: Polars
+    },
+    PANDAS: {
+        name: "Pandas",
+        icon: Pandas
+    },
+    SQLALCHEMY: {
+        name: "SQLAlchemy",
+        icon: SQLAlchemy
+    },
+    BEAUTIFULSOUP: {
+        name: "BeautifulSoup",
+        icon: BeautifulSoup
+    },
+    PYDANTIC: {
+        name: "Pydantic",
+        icon: Pydantic,
+    }
+}
